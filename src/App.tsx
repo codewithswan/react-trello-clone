bb342ac00df2b9780da3eaf3ea34988e446a5e3c
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2020-09-26 15:23:37 +0200

    lesson-1 basic list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import { colors, rounded } from "./styles/constants";
+
+export const StyledBoard = styled.div`
+  display: flex;
+  overflow-x: scroll;
+  background-color: ${colors.lightBlue};
+  height: 100vh;
+  padding-left: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  align-items: flex-start;
+  color: ${colors.gray900};
+`;
+
+const StyledList = styled.div`
+  background-color: ${colors.gray200};
+  width: 276px;
+  border-radius: ${rounded.max};
+  margin-right: 12px;
+  padding: 10px;
+`;
+
+const ListHeading = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+export const StyledCard = styled.div`
+  background-color: ${colors.white};
+  box-shadow: 0 1px 0 rgba(9, 30, 66, 0.4);
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: ${rounded.mid};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledBoard>
+      <StyledList>
+        <ListHeading>To do</ListHeading>
+
+        <StyledCard>Hello 1</StyledCard>
+        <StyledCard>Hello 2</StyledCard>
+        <StyledCard>Hello 3</StyledCard>
+      </StyledList>
+      <StyledList>
+        <ListHeading>In Progress</ListHeading>
+
+        <StyledCard>Hello 3</StyledCard>
+      </StyledList>
+    </StyledBoard>
   );
 }
 
---
+++ src/styles/constants.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  white: "#ffffff",
+  gray100: "#fafafa",
+  gray200: "#f2f2f2",
+  gray300: "#dedede",
+  gray500: "#555",
+  gray900: "#333",
+  lightBlue: "#04E1FF",
+};
+
+export const rounded = {
+  max: "8px",
+  mid: "4px",
+};

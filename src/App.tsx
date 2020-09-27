3b7a72c8f9dc7997c14748647f865f60001b4627
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2020-09-27 20:23:08 +0200

    lesson-3: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import styled from "styled-components";
 import { Board } from "./Board";
-import { colors, rounded } from "./styles/constants";
 
 function App() {
   return <Board />;

232ea77d94bec693d930da8cc80f9766ffca41d5
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2020-11-21 21:06:45 +0200

    Bound to data from the server

--- src/state/types.ts
+++ src/state/types.ts
@@ -1,3 +1,5 @@
+import { DropResult } from "react-beautiful-dnd";
+
 export interface CardData {
   text: string;
   id: string;
@@ -15,3 +17,34 @@ export interface BoardData {
   addingOnList: string | undefined;
   lists: { [key: string]: ListData };
 }
+
+export interface StartAddAction {
+  type: "startAdd";
+  listId: string;
+}
+
+export interface AddCancel {
+  type: "addCancel";
+}
+
+export interface ConfirmAddAction {
+  type: "confirmAdd";
+  text: string;
+}
+
+export interface DragEndAction {
+  type: "dragEnd";
+  result: DropResult;
+}
+
+export interface FetchBoardsSuccessAction {
+  type: "fetchBoards/fulfilled";
+  payload: BoardData[];
+}
+
+export type BoardAction =
+  | StartAddAction
+  | AddCancel
+  | ConfirmAddAction
+  | DragEndAction
+  | FetchBoardsSuccessAction;
